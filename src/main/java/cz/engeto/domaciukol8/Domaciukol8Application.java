a640d070adbd78ad88b0fedbb88ae39a536f9530
Zuzana Tománková <Tomankova.Z@seznam.cz>
2024-03-10 14:08:58 +0100

    domácí úkol8

---
+++ src/main/java/cz/engeto/domaciukol8/Domaciukol8Application.java
@@ -0,0 +1,13 @@
+package cz.engeto.domaciukol8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Domaciukol8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Domaciukol8Application.class, args);
+	}
+
+}
---
+++ src/test/java/cz/engeto/domaciukol8/Domaciukol8ApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.engeto.domaciukol8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Domaciukol8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
